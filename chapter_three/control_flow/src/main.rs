e8aa7c50f1bb3eb80e34ac5792aaa12a4fb9bd31
Babatunde AbdulHameed <humiditii45@gmail.com>
2024-01-23 12:32:19 +0100

    more stuff

--- chapter_three/control_flow/src/main.rs
+++ chapter_three/control_flow/src/main.rs
@@ -14,4 +14,34 @@ fn main() {
     let paid: bool = if amount_paid > 0 {true} else {false};
 
     println!("paid status: {paid}");
+
+    let m_a:i32 = do_something();
+    
+    println!("the function ran and returned this: {m_a}");
+}
+
+
+fn do_something() -> i32 {
+    let mut age: i32 = 24;
+
+    let result = loop {
+        println!("the age is {age}");
+        
+        while age != 0 {
+
+            println!("we will try to decrease the age: {age}");
+
+            age = age - 1;
+        }
+
+        if age == 0 {
+            println!("Age is now zero");
+        }
+
+        let reset_age: i32 = if age == 0 {24} else {1};
+
+        break reset_age;
+    };
+
+    return result;
 }
